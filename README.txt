43d60cbac5291720e75b10b47f0e5d834f00be1b
Ian VanDooren <63620074+Ian-VanDooren@users.noreply.github.com>
2021-02-16 12:56:49 -0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+Meal finder built with vanilla HTML, CSS and JavaScript. Using Traversy media tutorial. 
+Some basic fetch API integration
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const search = document.getElementById('search'),
     submit = document.getElementById('submit'),
-    random = document.getElementById('randome'),
+    random = document.getElementById('random'),
     mealsEl = document.getElementById('meals'),
     resultHeading = document.getElementById('result-heading'),
     single_mealEL = document.getElementById('single-meal');
@@ -56,6 +56,25 @@ function getMealById(mealID) {
         addMealToDOM(meal);
     });
 }
+
+// Fetch random meal from API
+function getRandomMeal() {
+    //Clear Meals and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then(res => res.json())
+    .then(data => {
+        const meal = data.meals[0];
+
+        addMealToDOM(meal);
+
+    })
+};
+
+
+
 //Add meal to DOM
 function addMealToDOM(meal) {
    const ingredients = [];
@@ -90,6 +109,8 @@ function addMealToDOM(meal) {
 
 //Event Listeners
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
+
 
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
